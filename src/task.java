ea083cc558faba9c861632551690c762f0c2f18f
zeynepmerdogan <zeynepmelekerdogan@gmail.com>
2020-11-24 20:33:27 +0100

    sepete ekle kismi eklendi

--- src/task.java
+++ src/task.java
@@ -2,6 +2,11 @@ public class task {
 
     public static void main(String[] args) {
         System.out.println("1. islem");
-    }
 
+    }
+    public static void  yeniMethod()
+    {
+        System.out.println("yeni metodum hazir");
+        System.out.println("sepete ekle kisimlari buralar");
+    }
 }
